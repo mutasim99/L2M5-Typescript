0ea30367c57f7884f15925ce4fcede76916b5b71
mutasim99 <virginmr882@gmail.com>
2025-11-09 16:30:36 +0600

    type alias

---
+++ src/typeAlias.ts
@@ -0,0 +1,23 @@
+type User = {
+    id: number,
+    name: {
+        firstName: string,
+        lastName: string,
+    };
+};
+
+const user1: User = {
+    id: 123,
+    name: {
+        firstName: 'Mutasim',
+        lastName: 'Billah'
+    }
+};
+
+const user2: User = {
+    id: 236,
+    name:{
+        firstName:'tasnim',
+        lastName:'Billah'
+    }
+}
